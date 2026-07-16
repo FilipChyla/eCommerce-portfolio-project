a3c76e51c36bf525747f0833661b3a8b39832310
FilipChyla <96245628+FilipChyla@users.noreply.github.com>
2026-07-16 20:36:49 +0200

    Refactor `UserPrincipal` to use a Java record for immutability.

--- src/main/java/io/github/filipchyla/shopapi/security/UserPrincipal.java
+++ src/main/java/io/github/filipchyla/shopapi/security/UserPrincipal.java
@@ -1,17 +1,13 @@
 package io.github.filipchyla.shopapi.security;
 
 import io.github.filipchyla.shopapi.user.User;
-import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-@AllArgsConstructor
-public class UserPrincipal implements UserDetails {
-    private final User user;
-
+public record UserPrincipal(User user) implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
