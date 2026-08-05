009508624431efe8aacc9b6d443de355b7fc5776
FilipChyla <f.chyla12@gmail.com>
2026-08-05 22:47:29 +0200

    Add @ActiveProfiles("test") to enable test-specific configurations in GitHub actions. Add Redis host and port to example .env file.

--- src/test/java/io/github/filipchyla/shopapi/AuthControllerIntegrationTest.java
+++ src/test/java/io/github/filipchyla/shopapi/AuthControllerIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class AuthControllerIntegrationTest {
     @Autowired
