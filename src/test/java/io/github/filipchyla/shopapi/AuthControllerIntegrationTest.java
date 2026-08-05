5dd03e4fd91a2b30ebd82612e4c5eeec6ef43aca
FilipChyla <f.chyla12@gmail.com>
2026-08-05 23:09:52 +0200

    Fix test profiles. Different profiles for h2 and testcontainers.

--- src/test/java/io/github/filipchyla/shopapi/AuthControllerIntegrationTest.java
+++ src/test/java/io/github/filipchyla/shopapi/AuthControllerIntegrationTest.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
-@ActiveProfiles("test")
+@ActiveProfiles({"test", "testcontainers"})
 @AutoConfigureMockMvc
 class AuthControllerIntegrationTest {
     @Autowired
