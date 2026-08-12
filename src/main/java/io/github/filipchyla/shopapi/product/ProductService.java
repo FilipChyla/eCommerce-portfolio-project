4558cb8d4cd9a3bf64dd89a976c467ab1f806b46
FilipChyla <f.chyla12@gmail.com>
2026-08-12 02:11:52 +0200

    Add product filtering and pagination with specifications, DTO response, and validation

--- src/main/java/io/github/filipchyla/shopapi/product/ProductService.java
+++ src/main/java/io/github/filipchyla/shopapi/product/ProductService.java
@@ -4,8 +4,12 @@ import io.github.filipchyla.shopapi.product.category.Category;
 import io.github.filipchyla.shopapi.product.category.CategoryService;
 import io.github.filipchyla.shopapi.product.dto.CreateProductRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Service
@@ -14,6 +18,16 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final CategoryService categoryService;
 
+    public Page<Product> findProducts(UUID categoryId, BigDecimal minPrice,
+                                              BigDecimal maxPrice, Pageable pageable) {
+        if (minPrice != null && maxPrice != null && minPrice.compareTo(maxPrice) > 0) {
+            throw new IllegalArgumentException("minPrice must be smaller than maxPrice");
+        }
+
+        Specification<Product> spec = ProductSpecification.filterBy(categoryId, minPrice, maxPrice);
+
+        return productRepository.findAll(spec, pageable);
+    }
     public Product addProduct(CreateProductRequest newProduct) {
         Product product = new Product();
         product.setName(newProduct.name());
