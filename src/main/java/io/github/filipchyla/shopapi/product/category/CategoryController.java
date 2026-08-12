3ac5816acfe20994ebaf387ab539d7787d6834e1
FilipChyla <f.chyla12@gmail.com>
2026-08-12 13:39:33 +0200

    Add role-based authorization for product and category management actions.

--- src/main/java/io/github/filipchyla/shopapi/product/category/CategoryController.java
+++ src/main/java/io/github/filipchyla/shopapi/product/category/CategoryController.java
@@ -6,6 +6,7 @@ import io.github.filipchyla.shopapi.product.category.dto.UpdateCategoryRequest;
 import io.github.filipchyla.shopapi.shared.dto.MessageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,18 +25,21 @@ public class CategoryController {
         return ResponseEntity.ok(categoryService.getCategoryTree());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<CategoryResponse> addCategory(@RequestBody CreateCategoryRequest categoryData) {
         CategoryResponse response = categoryMapper.toCategoryResponse(categoryService.addCategory(categoryData));
         return ResponseEntity.ok(response);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<CategoryResponse> updateCategory(@PathVariable UUID id, @RequestBody UpdateCategoryRequest categoryData) {
         CategoryResponse response = categoryMapper.toCategoryResponse(categoryService.updateCategory(id, categoryData));
         return ResponseEntity.ok(response);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<MessageResponse> deleteCategory(@PathVariable UUID id) {
         categoryService.deleteCategory(id);
