cef598cdac1b136c2f017dae2e5b4906b4f1270c
FilipChyla <f.chyla12@gmail.com>
2026-08-18 02:14:00 +0200

    Fix CreateProductRequest validation, change size to positiveOrZero for stockQuantity

--- src/main/java/io/github/filipchyla/shopapi/product/dto/CreateProductRequest.java
+++ src/main/java/io/github/filipchyla/shopapi/product/dto/CreateProductRequest.java
@@ -2,7 +2,7 @@ package io.github.filipchyla.shopapi.product.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.PositiveOrZero;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -14,7 +14,7 @@ public record CreateProductRequest(
         String description,
         @NotNull(message = "Product price should not be null")
         BigDecimal price,
-        @Size(message = "Product stock quantity should be positive")
+        @PositiveOrZero(message = "Product stock quantity should be positive")
         Integer stockQuantity,
         @NotNull(message = "Product category should not be null")
         UUID categoryId) {
